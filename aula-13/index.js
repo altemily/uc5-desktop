06e876bc16c2d15aa2f9799e026d6eb33b37aa66
altemily <altemilyc@gmail.com>
2025-04-07 16:18:29 -0300

    add: Adiciona aula 13 ao repositório.

---
+++ aula-13/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const alunoRoutes = require('./src/modules/aluno/routes/index')
+const enderecoRoutes = require('./src/modules/endereco/routes/index')
+dotenv.config();
+
+const port = process.env.PORTA;
+const app = express();
+
+app.use(express.json());
+
+app.use(alunoRoutes);
+app.use(enderecoRoutes);
+
+
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+  });
