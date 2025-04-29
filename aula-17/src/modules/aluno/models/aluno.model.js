edb3a2e399a51e38ffede720bea5a6c340dd9879
altemily <altemilyc@gmail.com>
2025-04-28 21:13:23 -0300

    feat: Implementa o Models, Controller e Routes para o módulo secretário.

---
+++ aula-17/src/modules/aluno/models/aluno.model.js
@@ -0,0 +1,64 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../../../config/configDB');
+
+const AlunoModel = sequelize.define('AlunoModel', {
+  matricula: {
+    type: DataTypes.CHAR(8),
+    allowNull: false,
+    primaryKey: true,
+    validate: {
+      is: {
+        args: /^[A-Za-z]\d{7}$/,
+        msg: 'A matrícula deve começar com uma letra seguida de 7 números!'
+      }
+    }
+  
+    },
+    nome: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    email:{
+        type: DataTypes.STRING(60),
+        allowNull: false,
+        unique: true,
+        validate:{
+            isEmail:{
+                args: /^[A-Za-z0-9._%+-]+@edum\.rn\.senac\.br$/,
+                msg: 'Forneça um email válido!'
+            },
+            len:{
+                args:[10,60],
+                msg: 'O email deve ter no mínimo 10 caracteres e no máximo 60!'
+            }
+        }
+    },
+    senha: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+        validate:{
+            len:{
+                args:[10],
+                msg: 'A senha deve ter 10 caracteres!'
+            }
+        }
+    },
+    turma_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references:{
+            model: turma,
+            key: turma_cod
+        }
+    },
+  },
+
+  {
+    tableName: 'aluno',
+    createdAt: 'criado_em',
+    updatedAt: 'atualizado_em'
+  },
+);
+
+
+module.exports = AlunoModel;
